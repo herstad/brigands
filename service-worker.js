d164eaf3e770c761ae930cfb6c68e289e259a685
Martin Herstad <mherstad@spotify.com>
2019-08-15 20:20:02 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/brigands/precache-manifest.341068480d40922ba917e80346502b35.js"
+  "/brigands/precache-manifest.cfc1b7d4c109f68350ef4aad4499b789.js"
 );
 
 workbox.clientsClaim();
